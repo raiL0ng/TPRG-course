5c4f8e68323e0c57ee88ec853a7577104041cb72
raiL0ng <19nik.nikitov.97@mail.ru>
2023-03-19 21:51:52 +0400

    add 6/9

--- task1/generators.cpp
+++ task1/generators.cpp
@@ -4,35 +4,51 @@
 #include<cstring>
 #include<string>
 #include<fstream>
+#include<cmath>
 using namespace std;
 
 
-struct 
-{
+struct {
   string filename = "rnd.txt", method_code = "", i = "";
   int n = 10000;
 } Flags_inf;
 
 
-void linear_congruent_method(vector<int>& seq, vector<int>& p, int n=10000) 
-{
+void linear_congruent_method(vector<int>& seq, vector<int>& p, int n=10000) {
   seq.push_back(p[3]);
-  for (int i = 1; i < n; i++) 
-  {
+  for (int i = 1; i < n; i++) {
     seq.push_back((p[1] * seq[i - 1] + p[2]) % p[0]);
   }
 }
 
 
-void additive_method(vector<int>& seq, vector<int>& p, int n=10000) 
-{
+void additive_method(vector<int>& seq, vector<int>& p, int n=10000) {
   seq.push_back(p[p.size() - p[1]] % p[0]);
   seq.push_back(p[p.size() - p[2]] % p[0]);
   for (int i = 0; i < n; i++)
     seq.push_back((seq[i - 1] + seq[i - 2]) % p[0]);
 }
 
-void get_key_for_rc4(vector<int>& key, string num) {
+void five_parameter_method( vector<int>&seq, vector<int> xs, int p, int q1
+                          , int q2, int q3, int w, int n=10000 ) {
+  int num, posl = 0, posr;
+  string s;
+  vector<int> nums;
+  for (int i = 1; i < n; i++) {
+    s = "";
+    posr = posl + w;
+    for (int j = posl; j < posr; j++) {
+    // TODO wtf
+      num = xs[j + q1] + xs[j + q2] + xs[j + q3] + xs[j];
+      s += to_string(num % 2);
+      // nums.push_back(num);
+      // posl++;
+    }
+    seq.push_back(stoi(s, nullptr, 2));
+  }
+  nums.clear();
+}
+void get_coeffs(vector<int>& key, string num) {
   for (int i = 0; i < num.length(); i++) {
     if (num[i] != '0' && num[i] != '1') {
       key.clear();
@@ -42,23 +58,69 @@ void get_key_for_rc4(vector<int>& key, string num) {
   }
 }
 
-void rc4(vector<int>& seq, int b_len, int n=10000) 
-{
-  vector<int> k, s(b_len);
-  get_key_for_rc4(k, Flags_inf.i);
+
+void get_binaryarr(vector<int>& x, int num) {
+  int d = 0;
+  for (int i = 0; i < 33; i++) {
+    if (pow(2, i) > num) {
+      d = i;
+      break;
+    }
+  }
+  for (int i = d - 1; i >= 0; i--) {
+    int k = num >> i;
+    if (k & 1)
+      x.push_back(1);
+    else
+      x.push_back(0);
+    }
+}
+
+
+void shift_arr(vector<int>& a, int new_el, char fl='l') {
+  if (fl == 'l') {
+    a.erase(a.begin());
+    a.push_back(new_el);
+  }
+  else {
+    vector<int> tmp;
+    tmp.push_back(new_el);
+    for (int i = 1; i < a.size(); i++)
+      tmp.push_back(a[i - 1]);
+    a = tmp;
+  }
+}
+
+
+void lfsr(vector<int>& seq, vector<int>& coefs, vector<int>& x, int n=10000) {
+  int num, len = x.size(), w = 10;
+  string s;
+  for (int k = 0; k < n; k++) {
+    s = "";
+    for (int j = 0; j < w; j++) {
+      num = coefs[0] * x[0];
+      for (int i = 1; i < len; i++)
+        num = !num != !(coefs[i] * x[i]);
+      s += to_string(num);
+      shift_arr(coefs, num, 'l');
+    }
+    seq.push_back(stoi(s, nullptr, 2));
+  }
+}
+
+void rc4(vector<int>& seq, vector<int>& k, int b_len, int n=10000) {
+  vector<int> s(b_len);
   if (k.empty()) {
     return;
   }
   for (int i = 0; i < b_len; i++) s[i] = i;
   int i, j = 0, t;
-  for (i = 0; i < b_len; i++) 
-  {
+  for (i = 0; i < b_len; i++) {
     j = (j + s[i] + k[i]) % b_len;
     swap(s[i], s[j]);
   }
   i = 0, j = 0;
-  for (int p = 0; p < n; p++) 
-  {
+  for (int p = 0; p < n; p++) {
     i = (i + 1) % b_len;
     j = (j + s[i]) % b_len;
     swap(s[i], s[j]);
@@ -68,8 +130,7 @@ void rc4(vector<int>& seq, int b_len, int n=10000)
 }
 
 
-int get_bitrait (int i) 
-{
+int get_bitrait (int i) {
   int n = 1;
   while (i > 1) {
       n *= 10;
@@ -78,16 +139,14 @@ int get_bitrait (int i)
   return n;
 }
 
-bool check_prime(int n) 
-{
+bool check_prime(int n) {
   if (n == 0 || n == 1) return false;
   for (int i = 2; i < n / 2; i++)
     if (n % i == 0) return false;
   return true;
 }
 
-int gen_rand_num(int upp, int low)
-{
+int gen_rand_num(int upp, int low) {
   srand((unsigned) time(NULL));
   int n = (rand() % (upp - low + 1)) + low;
   while (!check_prime(n))
@@ -95,11 +154,9 @@ int gen_rand_num(int upp, int low)
   return n;
 }
 
-long long power(long long base, long long exp, int mod) 
-{
+long long power(long long base, long long exp, int mod) {
    long long res = 1;
-   while (exp > 0) 
-   {
+   while (exp > 0) {
       if (exp % 2 == 1)
          res= (res * base) % mod;
       exp = exp >> 1;
@@ -108,20 +165,17 @@ long long power(long long base, long long exp, int mod)
    return res;
 }
 
-void rsa(vector<int>& seq, int n, int e, int x0, int l=10000)
-{
+void rsa(vector<int>& seq, int n, int e, int x0, int l=10000) {
   vector<int> nums;
   int num, bit, posl = 0, posr, b = 10;
   string s;
   cout << "Input parameters:\n";
   cout << "n=" << n << " e=" << e << " x=" << x0 << endl;
   nums.push_back(x0);
-  for (int i = 1; i < l; i++)
-  {
+  for (int i = 1; i < l; i++) {
     s = "";
     posr = posl + b;
-    for (int j = posl; j < posr; j++) 
-    {
+    for (int j = posl; j < posr; j++) {
       num = power(nums[j - 1], e, n);
       s += to_string(num & (1 << 0));
       nums.push_back(num);
@@ -132,18 +186,15 @@ void rsa(vector<int>& seq, int n, int e, int x0, int l=10000)
   nums.clear();
 }
 
-void blum_blum_shub_algo(vector<int>& seq, int x0, int l=10000)
-{
+void blum_blum_shub_algo(vector<int>& seq, int x0, int l=10000) {
   int num, n = 16637, posl = 0, posr, b = 10;
   string s;
   vector<int> nums;
   nums.push_back(x0);
-  for (int i = 1; i < l; i++)
-  {
+  for (int i = 1; i < l; i++) {
     s = "";
     posr = posl + b;
-    for (int j = posl; j < posr; j++) 
-    {
+    for (int j = posl; j < posr; j++) {
       num = power(nums[j - 1], 2, n);
       s += to_string(num & (1 << 0));
       nums.push_back(num);
@@ -155,8 +206,7 @@ void blum_blum_shub_algo(vector<int>& seq, int x0, int l=10000)
 }
 
 
-int convert_parameters(string& s) 
-{
+int convert_parameters(string& s) {
   int num = stoi(s.substr(0, s.find(",")));
   string tmp = s.substr(s.find(",") + 1);
   s = tmp;
@@ -178,8 +228,7 @@ void find_multiply_of_number(int aM1, int m) {
 }
 
 
-vector<int> define_method(string code) 
-{
+vector<int> define_method(string code) {
   vector<int> seq;
   if (code == "lc") {
     vector<int> p(4);
@@ -232,7 +281,20 @@ vector<int> define_method(string code)
     
   }
   if (code == "lfsr") {
+    int x;
+    vector<int> c, xs;
+    string params = Flags_inf.i;
+    get_coeffs(c, params.substr(0, params.find(",")));
+    x = convert_parameters(params);
+    x = convert_parameters(params);
+    get_binaryarr(xs, x);
+    // TODO подумать над ограничениями
+    if (xs.size() > c.size())
+      return seq;
+    for (int el : c) cout << el; cout<<endl;
+    for (int el : xs) cout << el; cout<<endl;
     
+    lfsr(seq, c, xs, Flags_inf.n);
   }
   if (code == "nfsr") {
     
@@ -241,10 +303,23 @@ vector<int> define_method(string code)
     
   }
   if (code == "rc4") {
-    rc4(seq, Flags_inf.i.length(), Flags_inf.n);
+    vector<int> p;
+    int num;
+    string params = Flags_inf.i;
+    p.push_back(convert_parameters(params));
+    p.push_back(convert_parameters(params));
+    for (int i = 2; i < 257; i++) {
+      int tmp = convert_parameters(params);
+      if ((tmp == p[i - 1] && tmp == p[i - 2])) {
+        p.pop_back();
+        break;
+      }
+      p.push_back(tmp);
+    }
+    if (p.size() > 256) p.pop_back();
+    rc4(seq, p, p.size(), Flags_inf.n);
   }
   if (code == "rsa") {
-    // TODO доделать параметры i и генерацию чисел
     int n, e, x;
     string params = Flags_inf.i;
     n = convert_parameters(params);
@@ -267,18 +342,6 @@ vector<int> define_method(string code)
 }
 
 
-bool check_parameters() 
-{
-  if (Flags_inf.method_code == "") {
-    return false;
-  }
-  else if (Flags_inf.i == "") {
-    return false;
-  }
-  return true;
-}
-
-
 void advert(string par="") {
   if (par == "") {
     cout << "The program has the following commands:\n";
@@ -291,11 +354,12 @@ void advert(string par="") {
     cout << "/i:<parameters> --- generator initialization vector.\n";
     cout << "parameters for methods:\n"
             "\t<X0,a,c,m> - parameters for \"lc\"\n"
-            "\t<k,j> - parameters for \"add\"\n\t<---> - parameters for \"5p\"\n"
-            "\t<---> - parameters for \"lfsr\"\n\t<---> - parameters for \"nfsr\"\n"
+            "\t<m,k,j,x_1,...x_55> - parameters for \"add\"\n"
+            "\t<p,q1,q2,q3,w,x_1...x_p> - parameters for \"5p\"\n"
+            "\t<coeffs,num> - parameters for \"lfsr\"\n\t<---> - parameters for \"nfsr\"\n"
             "\t<---> - parameters for \"mt\"\n"
-            "\t<key> - parameter for \"rc4\". key - it's 0 or 1 numbers (1 < their length< 257)\n"
-            "\t<---> - parameters for \"rsa\"\n\t<---> - parameters for \"bbs\"\n";
+            "\t<seq> - parameter for \"rc4\". seq - sequence of numbers (1 < sequence length< 257)\n"
+            "\t<n,e,x> - parameters for \"rsa\"\n\t<x> - parameter for \"bbs\"\n";
     cout << "/n:<length> --- amount of generated numbers.\n\n";
     cout << "/f:<complete_file_name> --- to output generated numbers to file.\n\n";
     cout << "/h --- complete information about commands.\n\n";
@@ -312,7 +376,10 @@ void advert(string par="") {
 
   }
   else if (par == "lfsr") {
-
+    //TODO
+    cout << "The following parameters must be entered for correct operation:\n\n";
+    cout << "/i:<coeffs,num> - coeffs - sequens of 0 and 1, num - decimal number\n";
+    cout<< "Also you can use /n:<length> to set length of the sequence of numbers 0 < n < 10001\n";
   }
   else if (par == "nfsr") {
 
@@ -322,16 +389,37 @@ void advert(string par="") {
   }
   else if (par == "rc4") {
     cout << "The following parameters must be entered for correct operation:\n\n";
-    cout << "/i:<sequence of bits> - sequence of bits have length more then 0 and less then 257\n";
+    cout << "/i:<sequence of numbers> - sequence of numbers have length more then 0 and less then 257\n";
     cout<< "Also you can use /n:<length> to set length of the sequence of numbers 0 < n < 10001\n";
   }
   else if (par == "rsa") {
-
+    cout << "The following parameters must be entered for correct operation:\n\n";
+    cout << "/i:<n,e,x> - where n - module, e - number for to exponentiate," 
+            " x - initial number (0 < x < n + 1).\n";
+    cout<< "Also you can use /n:<length> to set length of the sequence of numbers 0 < n < 10001\n";
   }
   else if (par == "bbs") {
-
+    cout << "The following parameters must be entered for correct operation:\n\n";
+    cout << "/i:<x> - where x - initial number (0 < x < n + 1) and x must be GCD(x, 16637) = 1.\n";
+    cout<< "Also you can use /n:<length> to set length of the sequence of numbers 0 < n < 10001\n";
   }
 }
+
+
+bool check_parameters() 
+{
+  if (Flags_inf.method_code == "") {
+    cout << "Incorrect data! Please enter \'/h\' to see information\n";
+    return false;
+  }
+  else if (Flags_inf.method_code != "" && Flags_inf.i == "") {
+    cout << "Incorrect data! Please enter \'/h\' to see information\n";
+    advert(Flags_inf.method_code);
+    return false;
+  }
+  return true;
+}
+
 
 int main(int argc, char* argv[]) {
   string s;
@@ -376,14 +464,12 @@ int main(int argc, char* argv[]) {
       }
         break;
       default: {
-
         cout << "Something wrong... Please enter \'/h\' to see information\n";
         return 0;
       }
     }
   }
   if (!check_parameters()) {
-    cout << "Incorrect data! Please enter \'/h\' to see information\n";
     return 0;
   }
   vector<int> seq;
@@ -395,8 +481,7 @@ int main(int argc, char* argv[]) {
   }
   ofstream out;
   out.open(Flags_inf.filename);
-  if (out.is_open()) 
-  {
+  if (out.is_open()) {
     for (int el : seq) 
       out << el << ' ';
   }
